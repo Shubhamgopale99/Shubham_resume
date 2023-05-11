7eb7b754accf5f5891214dc47234f6ddfb1307f5
Shubhamgopale99 <shubhamgopale0@gmail.com>
2023-05-11 21:26:23 +0530

    Shubham resume 2023 date :- 11/05/2023

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "index.html")
+
+def portfolio_details(request):
+    return render(request, "portfolio_details.html")
+# Create your views here.
